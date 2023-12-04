e4176e98427ecddc38cf2439da8e114e77e07d26
Zuwairanajma <juwairiyyasadiq@gmail.com>
2023-12-04 20:34:36 +0100

    Switch branch for descriptive purpose

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   # get "up" => "rails/health#show", as: :rails_health_check
   root 'static#index' # assuming 'static#index' is your static view
-
+  #get api random greetings
   get '/api/random_greeting', to: 'greetings#random', as: 'random_greeting'
   # Defines the root path route ("/")
   # root "posts#index"
